5fa4cc7822fd8d15b98cf6d7fd34a369ec1670fa
Crowdbotics <team@crowdbotics.com>
2021-03-11 06:59:00 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/raid_agents_24970/MainActivity.java
@@ -0,0 +1,15 @@
+package com.raid_agents_24970;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "raid_agents_24970";
+    }
+}
---
+++ src/config/app.js
@@ -0,0 +1,5 @@
+export const appConfig = {
+  // todo add library to handle env variables
+  emailAuthAPIEndPoint: "https://raid-agents-24970.botics.co",
+  defaultTimeout: 5000
+};
